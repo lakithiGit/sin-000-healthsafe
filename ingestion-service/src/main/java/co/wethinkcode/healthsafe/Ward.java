5c4c4fc2949c696df262417c5d05e65d7dd89c1c
TheAffiliate <nhngwjhb025@student.wethinkcode.co.za>
2026-09-22 13:31:11 +0200

    Ward java class created along with fields and methods

--- ingestion-service/src/main/java/co/wethinkcode/healthsafe/Ward.java
+++ ingestion-service/src/main/java/co/wethinkcode/healthsafe/Ward.java
@@ -1,1 +1,31 @@
+package co.wethinkcode.healthsafe;
 
+public class Ward {
+    private String wardId;
+    private String wing;
+    private String department;
+    private Integer bedsAvailable;
+
+    public Ward(String wardId, String wing, String department, Integer bedsAvailable) {
+        this.wardId = wardId;
+        this.wing = wing;
+        this.department = department;
+        this.bedsAvailable = bedsAvailable;
+    }
+
+    public String getWardId() {
+        return wardId;
+    }
+
+    public String getWing() {
+        return wing;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public Integer getBedsAvailable() {
+        return bedsAvailable;
+    }
+}
