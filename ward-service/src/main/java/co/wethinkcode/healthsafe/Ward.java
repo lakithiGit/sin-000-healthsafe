ff5bdd9c70b13dc279428203de2701ac88415245
TheAffiliate <nhngwjhb025@student.wethinkcode.co.za>
2026-09-22 14:34:33 +0200

    Ward class created along with fields and methods

--- ward-service/src/main/java/co/wethinkcode/healthsafe/Ward.java
+++ ward-service/src/main/java/co/wethinkcode/healthsafe/Ward.java
@@ -1,1 +1,28 @@
+package co.wethinkcode.healthsafe;
 
+public class Ward {
+
+    private String wardId;
+    private String wing;
+    private String department;
+    private Integer bedsAvailable;
+
+    public Ward() {
+    }
+
+    public String getWardId() {
+        return wardId;
+    }
+
+    public String getWing() {
+        return wing;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public Integer getBedsAvailable() {
+        return bedsAvailable;
+    }
+}
